8a45f5d12fa82acfa0cffe2c33b60c1baf5bf054
tivereidoro <tivereidoro@yahoo.com>
2024-08-25 23:57:04 +0100

    Bugs bugs buggy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import logo from "./assets/logo.png";
 
 export default function App() {
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400 app_container">
-      <header className="flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md">
+    <div className="w-full h-full absolute bg-gradient-to-r from-blue-400 to-emerald-400">
+      <header className="flex justify-between items-center text-black py-6 px-8 md:px-32 bg-gray-400 drop-shadow-md">
         <a href="#">
-          <img src={logo} alt="header-logo" srcset="" className="w-52 hover:scale-105 transition-all" />
+          <img src={logo} alt="header-logo" className="w-52 hover:scale-100 transition-all" />
         </a>
 
         <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
@@ -16,8 +19,20 @@ export default function App() {
           <li className="p-3 hover:bg-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Contact</li>
         </ul>
 
-        <div className="relative hidden md:flex items-center justify-center gap-3">
+        <div className="relative invisible md:flex items-center justify-center gap-3">
+          <i className="bx bx-search absolute left-3 text-2xl text-gray-500"></i>
+          <input type="text" className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500" placeholder="Search .." />
+        </div>
 
+        <i className="bx bx-menu xl:hidden block text-5xl cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+
+        <div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`} style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }}>
+          <ul>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Home</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Products</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Explore</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Contact</li>
+          </ul>
         </div>
       </header>
 
